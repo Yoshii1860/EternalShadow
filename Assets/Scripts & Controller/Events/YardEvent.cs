783b465d85b74bfccfbd4a1f8c1127798a9cc553
Yoshii1860 <andreas@antoniol.de>
2024-01-10 16:51:14 -0600

    Gathering lights into a new parent, adjusting reflection probes and refreshing it manually (on yard event)

--- Assets/Scripts & Controller/Events/YardEvent.cs
+++ Assets/Scripts & Controller/Events/YardEvent.cs
@@ -7,7 +7,11 @@ public class YardEvent : MonoBehaviour
     [SerializeField] GameObject girl;
     [SerializeField] GameObject gate;
     [SerializeField] Door door;
+    [SerializeField] GameObject yardLights;
+    [SerializeField] GameObject allLights;
     [SerializeField] GameObject light;
+    [SerializeField] ReflectionProbe[] reflectionProbes;
+    [SerializeField] GameObject[] alertLights;
     bool unique = true;
 
     void OnTriggerExit(Collider other) 
@@ -16,6 +20,11 @@ public class YardEvent : MonoBehaviour
         {
             unique = false;
             GameManager.Instance.GameplayEvent();
+            foreach (FlickeringLight fl in yardLights.GetComponentsInChildren<FlickeringLight>())
+            {
+                fl.enabled = true;
+            }
+            light.GetComponent<FlickeringLight>().smoothing = 5;
             gate.transform.eulerAngles = new Vector3(0, 0, 0);
             StartCoroutine(RotateDoor());
         }
@@ -29,9 +38,35 @@ public class YardEvent : MonoBehaviour
         yield return new WaitForSeconds(3f);
         door.CloseDoor();
         yield return new WaitForSeconds(1.5f);
-        light.SetActive(false); 
+        Lightning();
         girl.GetComponent<Animator>().SetBool("Walking", false);
         girl.SetActive(false);
         GameManager.Instance.ResumeGame();
+    }
+
+    // turn off all lights and reset the reflectionProbes to adjust to darkness
+    void Lightning()
+    {
+        foreach (Light l in allLights.GetComponentsInChildren<Light>())
+        {
+            l.enabled = false;
+        }
+        light.GetComponent<Light>().color = Color.red;
+        light.GetComponent<Light>().enabled = true;
+        light.GetComponent<FlickeringLight>().enabled = false;
+        for (int i = 0; i < alertLights.Length; i++)
+        {
+            alertLights[i].GetComponent<Light>().enabled = true;
+        }
+        StartCoroutine(ActivateProbesGradually());
+    }
+
+    IEnumerator ActivateProbesGradually()
+    {
+        for (int i = 0; i < reflectionProbes.Length; i++)
+        {
+            reflectionProbes[i].RenderProbe();
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
