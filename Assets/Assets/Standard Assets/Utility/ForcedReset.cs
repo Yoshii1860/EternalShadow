079e38b9ddf9675314d4b370e914ffb7eb4fc4df
Yoshii1860 <andreas@antoniol.de>
2024-05-12 18:42:23 -0600

    Restructuring folders

---
+++ Assets/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof (Image))]
+public class ForcedReset : MonoBehaviour
+{
+    private void Update()
+    {
+        // if we have forced a reset ...
+        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        {
+            //... reload the scene
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        }
+    }
+}
